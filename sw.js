86e0c089804c817b1af0a521c86a9694b7f5b8ec
giancarlo <giancarlopsk@gmail.com>
2019-05-05 12:41:49 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3b44323d0d3384e5ddc5.js"
+    "url": "webpack-runtime-acaab20f0b1a79361679.js"
   },
   {
     "url": "app-bbe1f7b2d86c03c6a83d.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "fa637a400128c1ee334e8ca5e6c622e5"
+    "revision": "e26f41803fe7f13a90c46cf95c3a7bc8"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
